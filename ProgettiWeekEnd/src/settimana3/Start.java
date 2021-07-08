364164fe0eacb5f96d2d0dc63dc3bf8604376114
LucaB <LucaB@host.docker.internal>
2021-07-08 15:29:54 +0200

    average aggiunto

--- ProgettiWeekEnd/src/settimana3/Start.java
+++ ProgettiWeekEnd/src/settimana3/Start.java
@@ -1,6 +1,7 @@
 package settimana3;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -18,7 +19,7 @@ public class Start {
 		Impiegato b = new Impiegato("Ciccio", "Binotto", Sesso.M, new BigDecimal(18000));
 		Impiegato c = new Impiegato("Sara", "Binotto", Sesso.F, new BigDecimal(45000));
 		Impiegato d = new Impiegato("Albert", "Binotto", Sesso.M, new BigDecimal(60000));
-		Impiegato e = new Impiegato("Lucia", "Binotto", Sesso.F, new BigDecimal(16000));
+		Impiegato e = new Impiegato("Lucia", "Binotto", Sesso.F, new BigDecimal(18000));
 		Impiegato f = new Impiegato("Enza", "Binotto", Sesso.F, new BigDecimal(25000));
 		Impiegato g = new Impiegato("Lucio", "Binotto", Sesso.M, new BigDecimal(30000));
 
@@ -32,8 +33,11 @@ public class Start {
 				
 		BigDecimal max = lista.stream().filter(p -> p.getSesso().equals(Sesso.M)).map(Impiegato::getStipendio).max(BigDecimal::compareTo).get();
 		BigDecimal min = lista.stream().filter(p -> p.getSesso().equals(Sesso.F)).map(Impiegato::getStipendio).min(BigDecimal::compareTo).get();
-		Double avg = lista.stream().map(Impiegato::getStipendio).collect(Collectors.averagingDouble(null));
+		BigDecimal avg = lista.stream().map(Impiegato::getStipendio).reduce((s, next) -> s.add(next)).map(s -> s.divide(BigDecimal.valueOf(lista.size()),RoundingMode.HALF_EVEN)).orElse(BigDecimal.ZERO);
+		//BigDecimal moda = lista.stream().map(Impiegato::getStipendio);
 
+		
+		
 		System.out.println(max);
 		System.out.println(min);
 		System.out.println(avg);
